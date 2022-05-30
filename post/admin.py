37df37b5ec6c496c80f85949a15b40d151bb1840
GGNE <z_zzlzl@naver.com>
2022-05-30 14:31:01 +0900

    좋아요, 북마크 기능 추가

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib import admin
-from .models import Post
-
+from .models import Post, Like, Bookmark
 
 
 # Register your models here.
@@ -13,12 +12,19 @@ class PostForm(forms.ModelForm):
         fields = '__all__'
 
 
+# 어드민 페이지 정리
+class LikeInline(admin.TabularInline):
+    model = Like
+
+
 '''
     - admin.register 데코레이터를 이용한다. 
     1. admin 페이지에서 id, author, nickname, content, created_at을 리스트에 보이게 하고, 
        author, nickname, content에만 링크가 걸리도록 설정한다. 
     2. nickname은 사용자가 포스트를 작성할 때 따로 입력하지 않기 때문에, nickname을 가져오는 함수를 만든다. 
 '''
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'nickname', 'content', 'created_at']
@@ -26,4 +32,16 @@ class PostAdmin(admin.ModelAdmin):
 
     def nickname(request, post):
         return post.author.profile.nickname
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post','user','created_at']
+    list_display_links = ['post', 'user']
+
+
+@admin.register(Bookmark)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post','user','created_at']
+    list_display_links = ['post', 'user']
 
--- post/views.py
+++ post/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .admin import PostForm
 from .models import Post
 
+import json
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+
 '''
     1. 요청이 들어오면 posts 변수에 Post 전부를 저장
     2. 사용자가 로그인 상태일 때, 사용자 이름을 저장 -> 유저 모델 내용을 확인 
@@ -93,4 +97,38 @@ def post_delete(request, pk):
     return redirect('post:post_list')
 
 
+@login_required
+@require_POST
+def post_like(request):
+    pk = request.POST.get('pk', None)
+    post = get_object_or_404(Post, pk=pk)
+    post_like, post_like_created = post.like_set.get_or_create(user=request.user)
 
+    if not post_like_created:
+        post_like.delete()
+        message = "좋아요를 취소합니다."
+    else:
+        message = "좋아요"
+
+    context = {'like_count': post.like_count, 'message': message }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+
+@login_required
+@require_POST
+def post_bookmark(request):
+    pk = request.POST.get('pk', None)
+    post = get_object_or_404(Post, pk=pk)
+    post_bookmark, post_bookmark_created = post.bookmark_set.get_or_create(user=request.user)
+
+    if not post_bookmark_created:
+        post_bookmark.delete()
+        message = "북마크 취소"
+    else:
+        message = "북마크"
+
+    context = {'bookmark_count': post.bookmark_count,
+               'message': message}
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
