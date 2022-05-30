3ae173a1589e7b268ef2443a38520db74cdd9f36
GGNE <z_zzlzl@naver.com>
2022-05-30 16:15:59 +0900

    팔로우 기능 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Follow
+
+
+# 팔로우 내용을 표 양식으로 볼 때 사용
+class FollowInline(admin.TabularInline):
+    model = Follow
+    fk_name = 'from_user'
+
 
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id', 'nickname', 'user']
     list_display_links = ['nickname', 'user']
-    search_fields = ['nickname']                     # 정보 검색
+    search_fields = ['nickname']  # 정보 검색
+
+
+@admin.register(Follow)
+class FollowAdin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'created_at']
+    list_display_links = ['from_user', 'to_user', 'created_at']
--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,7 @@ class Profile(models.Model):
                                   format='JPEG',
                                   options={'quality': 100},
                                   blank=True
-                                 )
+                                  )
 
     about = models.CharField(max_length=300, blank=True)
     GENDER_CHOICE = (
@@ -36,6 +36,56 @@ class Profile(models.Model):
         ('여성', '여성')
     )
     gender = models.CharField('성별(선택사항)', max_length=10, choices=GENDER_CHOICE, default='N')
+    follow_set = models.ManyToManyField('self',  # 자신을 참조
+                                        blank=True,  # 아무도 팔로우 안한 상태
+                                        through='Follow',  # 중간 모델
+                                        symmetrical=False)  # 비대칭 관계
 
     def __str__(self):
         return self.nickname
+
+    # 해당 유저를 팔로우하고 있는 유저
+    @property
+    def get_follower(self):
+        return [i.from_user for i in self.follower_user.all()]
+
+    # 해당 유저가 팔로우하고 있는 유저
+    @property
+    def get_following(self):
+        return [i.to_user for i in self.follow_user.all()]
+
+    @property
+    def follower_count(self):
+        return len(self.get_follower)
+
+    @property
+    def following_count(self):
+        return len(self.get_following)
+
+    def is_follower(self, user):
+        return user in self.get_follower
+
+    def is_following(self, user):
+        return user in self.get_following
+
+
+# Follow 모델
+class Follow(models.Model):
+    # Follow user
+    from_user = models.ForeignKey(Profile,
+                                  related_name='follow_user',
+                                  on_delete=models.CASCADE)
+    # Follower
+    to_user = models.ForeignKey(Profile,
+                                related_name='follower_user',
+                                on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # 관계가 언제 생겼는지 작성
+
+    # 인스턴스 추적 양식 지정
+    def __str__(self):
+        return '{} -> {}'.format(self.from_user, self.to_user)
+
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user')
+        )
